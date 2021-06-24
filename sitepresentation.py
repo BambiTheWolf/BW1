b281acc55e3505c6083566b2dc366e7b2c2acb57
Michael Redruello <redruellomichael@gmail.com>
2021-06-24 16:39:27 +0200

    Update sitepresentation.py

--- sitepresentation.py
+++ sitepresentation.py
@@ -12,7 +12,7 @@ st.title("Amsterdam's best business")
 #Sidebar
 st.sidebar.header("Presentation")
 st.sidebar.text("What is the best business in Amsterdam?")
-st.sidebar.markdown("""We are going to present a series of graphs to you now, extracted from [Yelp]("https://www.yelp.com/") to help you choose the best business to invest in""")
+st.sidebar.markdown("""We are going to present a series of graphs to you now, extracted from [Yelp]("https://www.yelp.it/search?find_desc=&find_loc=Amsterdam%2C+Noord-Holland&ns=1") to help you choose the best business to invest in""")
 
 #Main page/header
 st.header("How can you invest effectively")
